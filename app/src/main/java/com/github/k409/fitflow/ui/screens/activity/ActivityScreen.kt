9242930d14ed5d4b977bdc028b2144662b61a9e6
Mantelis <mantasjasikenas@gmail.com>
2024-02-16 21:56:12 +0200

    Add bottom navigation bar screens

---
+++ app/src/main/java/com/github/k409/fitflow/ui/screens/activity/ActivityScreen.kt
@@ -0,0 +1,22 @@
+package com.github.k409.fitflow.ui.screens.activity
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ActivityScreen(
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = "Activity Screen")
+    }
+}
