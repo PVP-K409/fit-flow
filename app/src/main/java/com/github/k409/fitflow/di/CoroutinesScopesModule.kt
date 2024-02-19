6d351f258440b92e70cfd4153ec0fe2a2f301f99
Mantelis <mantasjasikenas@gmail.com>
2024-02-19 13:06:30 +0200

    Add Hilt modules and annotations

---
+++ app/src/main/java/com/github/k409/fitflow/di/CoroutinesScopesModule.kt
@@ -0,0 +1,25 @@
+package com.github.k409.fitflow.di
+
+import com.github.k409.fitflow.di.annotations.ApplicationScope
+import com.github.k409.fitflow.di.annotations.DefaultDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesScopesModule {
+
+    @Singleton
+    @ApplicationScope
+    @Provides
+    fun providesCoroutineScope(
+        @DefaultDispatcher defaultDispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
+}
---
+++ app/src/main/java/com/github/k409/fitflow/di/FirebaseProviderModule.kt
@@ -0,0 +1,37 @@
+package com.github.k409.fitflow.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.PersistentCacheSettings
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.storage.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseProviderModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore() = Firebase.firestore.apply {
+        firestoreSettings = firestoreSettings {
+            setLocalCacheSettings(
+                PersistentCacheSettings.newBuilder()
+                    .build()
+            )
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage() = Firebase.storage
+}
