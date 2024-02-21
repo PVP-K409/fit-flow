1d779e1d2f3033cf31d3c9a5370b21289284908f
Mantelis <mantasjasikenas@gmail.com>
2024-02-21 15:10:17 +0200

    Setup app signing configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.daggerHilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.gms) apply false
 }
