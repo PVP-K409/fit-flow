c0f68179ae02ce0f435faacaab29d87d8837db77
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-02-28 19:42:22 +0200

    removed unused directives

--- app/src/main/java/com/github/k409/fitflow/ui/screens/activity/ActivityScreen.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/screens/activity/ActivityScreen.kt
@@ -3,8 +3,6 @@ package com.github.k409.fitflow.ui.screens.activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,17 +11,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.k409.fitflow.R
-import kotlinx.coroutines.launch
 
 @Composable
 fun ActivityScreen() {
     val activityViewModel: ActivityViewModel = hiltViewModel()
     val todaySteps = activityViewModel.todaySteps.observeAsState().value
-    val coroutineScope = rememberCoroutineScope()
-
 
     LaunchedEffect(key1 = Unit) {
         activityViewModel.updateTodayStepsManually()
