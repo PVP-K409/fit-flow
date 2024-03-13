7c51cf531fc4a7dc711e910574b6c7e6cf42417c
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-03-13 19:16:28 +0200

    Calories and distance are read from HealthConnect

--- app/src/main/java/com/github/k409/fitflow/di/AppModule.kt
+++ app/src/main/java/com/github/k409/fitflow/di/AppModule.kt
@@ -2,6 +2,9 @@ package com.github.k409.fitflow.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.health.connect.client.HealthConnectClient
+import com.github.k409.fitflow.di.services.HealthConnectService
+import com.github.k409.fitflow.features.stepcounter.CaloriesAndDistanceUtil
 import com.github.k409.fitflow.features.stepcounter.StepCounter
 import dagger.Module
 import dagger.Provides
@@ -23,5 +26,17 @@ class AppModule {
     @Singleton
     fun provideSharedPreferences(@ApplicationContext appContext: Context): SharedPreferences {
         return appContext.getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHealthConnectService(client: HealthConnectClient): HealthConnectService {
+        return HealthConnectService(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCaloriesAndDistanceutil(healthConnectService: HealthConnectService): CaloriesAndDistanceUtil{
+        return CaloriesAndDistanceUtil(healthConnectService)
     }
 }
