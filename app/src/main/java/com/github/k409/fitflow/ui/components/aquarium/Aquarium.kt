60fbba21d513f6c32be57a9761495d58855cfd5d
Mantelis <mantasjasikenas@gmail.com>
2024-03-19 19:03:08 +0200

    Add aquarium metrics card

--- app/src/main/java/com/github/k409/fitflow/ui/components/aquarium/Aquarium.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/components/aquarium/Aquarium.kt
@@ -2,13 +2,23 @@ package com.github.k409.fitflow.ui.components.aquarium
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WaterDrop
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
@@ -18,9 +28,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.github.k409.fitflow.R
 import com.github.k409.fitflow.model.AquariumFishType
 import com.github.k409.fitflow.ui.common.noRippleClickable
+import kotlin.math.roundToInt
 
 @Composable
 fun Aquarium(
@@ -32,6 +46,7 @@ fun Aquarium(
 
     val currentFish = remember { mutableIntStateOf(0) }
     val waterLevel = remember { mutableFloatStateOf(0.85f) }
+    val healthLevel = remember { mutableFloatStateOf(0.85f) }
 
     fun onDoubleClick() {
         currentFish.intValue = (currentFish.intValue + 1) % AquariumFishType.entries.size
@@ -74,28 +89,92 @@ fun Aquarium(
             Sand()
             Plant()
 
+            AquariumMetrics(
+                modifier = Modifier
+                    .align(Alignment.TopEnd),
+                waterLevel = waterLevel.floatValue,
+                healthLevel = healthLevel.floatValue
+            )
+
             Crossfade(
                 targetState = AquariumFishType.entries[currentFish.intValue],
                 label = "",
             ) {
                 Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Bottom,
                 ) {
                     DraggableFish(
                         modifier = Modifier
                             .fillMaxHeight(waterLevel.floatValue)
-                            .noRippleClickable(onDoubleClick = ::onDoubleClick)
-                            .border(
-                                width = 1.dp,
-                                color = Color.Red,
-                            ),
+                            .noRippleClickable(onDoubleClick = ::onDoubleClick),
+                        /* .border(
+                             width = 1.dp,
+                             color = Color.Red,
+                         )*/
                         fishSize = 100.dp,
                         fishDrawableId = it.imageRes,
                     )
                 }
 
+            }
+        }
+    }
+}
+
+@Composable
+fun AquariumMetrics(
+    modifier: Modifier = Modifier,
+    waterLevel: Float,
+    healthLevel: Float,
+) {
+    ElevatedCard(
+        modifier = modifier
+            .padding(horizontal = 20.dp, vertical = 50.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text(
+                    text = "${(waterLevel * 100).roundToInt()}%",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.width(6.dp))
+
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    imageVector = Icons.Filled.WaterDrop,
+                    contentDescription = "Water Level",
+                    tint = Color(0xFF7FABC0),
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text(
+                    text = "${(healthLevel * 100).roundToInt()}%",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.width(6.dp))
+
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.ecg_heart_24px),
+                    contentDescription = "Health",
+                    tint = Color(0xFFF44336),
+                )
             }
         }
     }
