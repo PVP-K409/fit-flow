75cbf60bef45c31d844110f1e8410fb6e0358f74
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-03-28 16:20:56 +0200

    removed wildcard import

--- app/src/main/java/com/github/k409/fitflow/ui/screens/goals/GoalCreation.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/screens/goals/GoalCreation.kt
@@ -1,6 +1,13 @@
 package com.github.k409.fitflow.ui.screens.goals
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
