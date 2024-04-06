21118f01579954edf484ec9cc95c2d4590bf3a7c
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-04-06 21:31:33 +0300

    added icons and more HealthConnect exercises

--- app/src/main/java/com/github/k409/fitflow/ui/screen/goals/GoalsScreen.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/screen/goals/GoalsScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.github.k409.fitflow.ui.common.HorizontalPagerIndicator
 
