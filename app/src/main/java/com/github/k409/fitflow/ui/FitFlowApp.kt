8db71d81d5acdb56cc9e22965a9832232ab3b9f3
Mantelis <mantasjasikenas@gmail.com>
2024-04-09 22:45:18 +0300

    Resize top bar title text

--- app/src/main/java/com/github/k409/fitflow/ui/FitFlowApp.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/FitFlowApp.kt
@@ -52,6 +52,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -133,10 +134,10 @@ fun FitFlowApp(
                     navController = navController,
                     currentScreen = currentScreen,
                     visible = !(
-                        navController.previousBackStackEntry != null && !NavRoutes.bottomNavBarItems.contains(
-                            currentScreen,
-                        )
-                        ) && bottomBarState.value,
+                            navController.previousBackStackEntry != null && !NavRoutes.bottomNavBarItems.contains(
+                                currentScreen,
+                            )
+                            ) && bottomBarState.value,
                     containerColor = if (currentScreen == NavRoutes.Aquarium) Color(0xFFE4C68B) else MaterialTheme.colorScheme.surface,
                 )
             },
@@ -210,6 +211,7 @@ fun FitFlowTopBar(
                 Text(
                     text = stringResource(id = currentRoute.stringRes),
                     style = MaterialTheme.typography.titleLarge,
+                    fontSize = 20.sp,
                 )
             },
             navigationIcon = {
