7a0fc9ac1645438e0a79c0cb7eeab3adb4496ae7
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-05-07 00:50:34 +0300

    exericse session data class

---
+++ app/src/main/java/com/github/k409/fitflow/model/ExerciseSession.kt
@@ -0,0 +1,18 @@
+package com.github.k409.fitflow.model
+
+import androidx.health.connect.client.records.ExerciseRoute
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
+
+data class ExerciseSession (
+    var exerciseType : Int = 0,
+    var startTime: Instant = Instant.now(),
+    var startZoneOffset: ZoneOffset = ZoneId.systemDefault().rules.getOffset(Instant.now()),
+    var endTime: Instant = Instant.now(),
+    var endZoneOffset: ZoneOffset = ZoneId.systemDefault().rules.getOffset(Instant.now()),
+    var distance: Float = 0.0f,
+    var calories: Long = 0L,
+    var route: List<ExerciseRoute.Location> = listOf(),
+    )
+
