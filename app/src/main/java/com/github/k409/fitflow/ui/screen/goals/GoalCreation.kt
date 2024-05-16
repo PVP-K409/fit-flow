6669c1dcef001b4b5fb390a1b873d2f5e63cb4e7
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-05-16 15:04:44 +0300

    fixed a bug where icons are not show correctly in exercise dropdown when localization is in Lithuanian

--- app/src/main/java/com/github/k409/fitflow/ui/screen/goals/GoalCreation.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/screen/goals/GoalCreation.kt
@@ -1,7 +1,5 @@
 package com.github.k409.fitflow.ui.screen.goals
 
-import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
