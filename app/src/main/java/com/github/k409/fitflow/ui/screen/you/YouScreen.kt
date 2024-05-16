310d448e201b3a4804ea8851b73164105bf3f58e
GillWell <erikasbutenas81@gmail.com>
2024-05-16 17:39:37 +0300

    Update YouScreen.kt

--- app/src/main/java/com/github/k409/fitflow/ui/screen/you/YouScreen.kt
+++ app/src/main/java/com/github/k409/fitflow/ui/screen/you/YouScreen.kt
@@ -57,7 +57,7 @@ fun YouScreen(
                 .verticalScroll(scrollState)
                 .padding(vertical = 16.dp)
                 .padding(bottom = 6.dp),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
         ) {
             when (selectedTabIndex.intValue) {
