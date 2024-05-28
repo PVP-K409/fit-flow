4f0bce383b19029fe9ebe857362059077b43f5a9
Mantelis <mantasjasikenas@gmail.com>
2024-05-28 21:05:19 +0300

    Refactor hydration notification receiving (#94)

--- app/src/main/java/com/github/k409/fitflow/model/Notification.kt
+++ app/src/main/java/com/github/k409/fitflow/model/Notification.kt
@@ -3,8 +3,8 @@ package com.github.k409.fitflow.model
 data class Notification(
     var id: Int = -1,
     val channel: NotificationChannel,
-    val title: String,
-    val text: String,
+    val title: String = "",
+    val text: String = "",
 ) {
     init {
         if (id == -1) {
